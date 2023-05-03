9f8dac6102c01664a11b8dcab59b2dc550699235
Mirtom <info@mirtom.it>
2023-05-03 23:02:26 +0200

    Initial Commit

---
+++ primaAppSwiftUI/primaAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  primaAppSwiftUIApp.swift
+//  primaAppSwiftUI
+//
+//  Created by nahmir on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct primaAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
